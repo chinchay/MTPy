995681b245906f812f3298c3c20704b9180841a3
Carlos Leon <cleonchinchay@wlu.ca>
2021-03-19 22:16:06 -0400

    Read information from relaxed structures in *.cfg MTP format
    After getting the fitted interatomic potential (zero configurations in relax step), perform one additional step:
    
    mlp relax relax.ini --cfg-filename=to_relax.cfg --min-dist=0.5 --save-relaxed=relaxedCfgs.cfg
    
    This `cfg2ase.py` is intended to collect information from relaxedCfgs.cfg

---
+++ cfg2ase.py
@@ -0,0 +1,74 @@
+from ase import Atom
+import numpy as np
+
+def formatify(string):
+    return [float(s) for s in string.split()]
+#
+
+def getSize(block):
+    size_pattern = re.compile("Size\n(.*?)\n SuperCell", re.S | re.I)
+    size_str = size_pattern.findall(block)[0]
+    return int(size_str.lstrip())
+#
+
+def getCell(block):
+    cell_pattern = re.compile("SuperCell\n(.*?)\n AtomData", re.S | re.I)
+    cell_str = cell_pattern.findall(block)[0]
+    return list(map(formatify, cell_str.split("\n")))
+#
+
+def getSpeciesPositionsForces(block):
+    """
+    species, positions, forces = getSpeciesPositionsForces(block)
+    """
+    position_pattern = re.compile("fz\n(.*?)\n Energy", re.S)
+    matrix_str = position_pattern.findall(block)[0]
+    matrix_floats = np.array(list(map(formatify, line_str.split("\n"))))
+    species   = np.array(self.elements)[line_floats[:, 1].astype(np.int64)]
+    positions = line_floats[:, 2:5]
+    forces    = line_floats[:, 5:8].tolist()
+    return species, positions, forces
+#
+
+def getEnergy(block):
+    energy_pattern = re.compile("Energy\n(.*?)\n (?=PlusStress|Stress)", re.S)
+    energy_str = energy_pattern.findall(block)[0]
+    return float(energy_str.lstrip())
+#
+
+def getStress(block):
+    stress_pattern = re.compile("xy\n(.*?)(?=\n|$)", re.S)
+    stress_str = stress_pattern.findall(block)[0]
+    virial_stress = formatify(stress_str)
+    return virial_stress
+#
+
+# inspired on ttps://github.com/materialsvirtuallab/maml/blob/master/maml/apps/pes/_mtp.py
+def read_cfgs(self, filename):
+    """
+    Args:
+        filename (str): The configuration file to be read.
+    """
+    with open(filename, "r") as f:
+        # lines = f.readlines() # << it won't work in the `for` loop :/
+        letters = f.read() # 
+    #
+    block_pattern = re.compile("BEGIN_CFG\n(.*?)\nEND_CFG", re.S)
+    
+    data_pool = []
+    for block in block_pattern.findall(letters):
+        d = {"outputs": {}}
+        d["size"] = getSize(block)
+        d["cell"] = getCell(block)
+        species, positions, forces = getSpeciesPositionsForces(block)
+        d["species"]   = species
+        d["positions"] = positions
+        assert d["size"] == len(species)
+        #
+        d["outputs"]["energy"] = energy
+        d["outputs"]["forces"] = forces
+        d["outputs"]["virial_stress"] = virial_stress
+        #
+        data_pool.append(d)
+    #
+    return data_pool
